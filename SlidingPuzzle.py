577950e87d9c32c4a2d29820c95306640984002a
aminrashidbeigi <aminrashidbeigi@aut.ac.ir>
2018-06-03 16:46:06 +0430

    sec commit

--- SlidingPuzzle.py
+++ SlidingPuzzle.py
@@ -7,7 +7,13 @@ class Problem:
             [0, 6, 8]
         ]
 
-    def isGoalTest(self, state):
-        return state == self.initialState()
+    def goal(self):
+        return [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+        ]
 
+    def isGoalTest(self, state):
+        return state == self.goal()
 
