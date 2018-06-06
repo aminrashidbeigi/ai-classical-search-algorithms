edb4c332da23fcf821113fb66dd07fa26b1ae533
aminrashidbeigi <aminrashidbeigi@aut.ac.ir>
2018-06-06 14:46:05 +0430

    print path added to SlidingPuzzle

--- SlidingPuzzle.py
+++ SlidingPuzzle.py
@@ -1,5 +1,7 @@
 from SearchAlgorithms.ClassicSearchAlgorithms import *
+from SearchAlgorithms.BeyondClassicSearchAlgorithm import *
 from copy import deepcopy
+
 
 class Problem():
     def initialState(self):
@@ -10,9 +12,9 @@ class Problem():
         # ]
 
         return [
-            [3, 1, 2],
-            [6, 4, 5],
-            [7, 0, 8]
+            [1, 2, 0],
+            [3, 4, 5],
+            [6, 7, 8]
         ]
 
     def goal(self):
@@ -35,19 +37,20 @@ class Problem():
                     zero_row = state.index(row)
                     zero_col = row.index(col)
         if zero_col > 0:
-            actions.append('Left')
+            actions.append('L')
         if zero_col < 2:
-            actions.append('Right')
+            actions.append('R')
         if zero_row > 0:
-            actions.append('Up')
+            actions.append('U')
         if zero_row < 2:
-            actions.append('Down')
+            actions.append('D')
         return actions
 
     def results(self, actions, state):
         zero_row = -1
         zero_col = -1
         states = []
+        result_actions = []
         for row in state:
             for col in row:
                 if col == 0:
@@ -55,27 +58,69 @@ class Problem():
                     zero_col = row.index(col)
         for action in actions:
             temp_state = deepcopy(state)
-            if 'Up' in action:
+            if 'U' in action:
                 temp_state[zero_row][zero_col], temp_state[zero_row - 1][zero_col] = state[zero_row - 1][zero_col], \
                                                                                      state[zero_row][zero_col]
-            if 'Down' in action:
+            if 'D' in action:
                 temp_state[zero_row][zero_col], temp_state[zero_row + 1][zero_col] = state[zero_row + 1][zero_col], \
                                                                                      state[zero_row][zero_col]
-            if 'Left' in action:
+            if 'L' in action:
                 temp_state[zero_row][zero_col], temp_state[zero_row][zero_col - 1] = state[zero_row][zero_col - 1], \
                                                                                      state[zero_row][zero_col]
-            if 'Right' in action:
+            if 'R' in action:
                 temp_state[zero_row][zero_col], temp_state[zero_row][zero_col + 1] = state[zero_row][zero_col + 1], \
                                                                                      state[zero_row][zero_col]
             states.append(temp_state)
-            # print(action)
-            # print(temp_state)
-
         return states
 
+    def step_cost(self, current_state, next_state):
+        return 1
+
+    def heuristic(self, state):
+        zero_row = -1
+        zero_col = -1
+        for row in state:
+            for col in row:
+                if col == 0:
+                    zero_row = state.index(row)
+                    zero_col = row.index(col)
+        return zero_col + zero_row
+
+    def print_path(self, path):
+        def find_direction(current_state, next_state):
+            current_zero_row = -1
+            current_zero_col = -1
+            for row in current_state:
+                for col in row:
+                    if col == 0:
+                        current_zero_row = current_state.index(row)
+                        current_zero_col = row.index(col)
+            next_zero_row = -1
+            next_zero_col = -1
+            for row in next_state:
+                for col in row:
+                    if col == 0:
+                        next_zero_row = next_state.index(row)
+                        next_zero_col = row.index(col)
+            if next_zero_col == current_zero_col and next_zero_row > current_zero_row:
+                print('D', end=" ")
+            if next_zero_col == current_zero_col and next_zero_row < current_zero_row:
+                print('U', end=" ")
+            if next_zero_col > current_zero_col and next_zero_row == current_zero_row:
+                print('R', end=" ")
+            if next_zero_col < current_zero_col and next_zero_row == current_zero_row:
+                print('L', end=" ")
+
+        print("Path:", end=" ")
+        for current_state, next_state in zip(path, path[1:]):
+            find_direction(current_state, next_state)
 
 p = Problem()
 csa = ClassicSearchAlgorithm(p)
-# csa.GraphDFS(p.initialState())
-# csa.GraphBFS(p.initialState())
-csa.bidirectional(p.initialState(), p.goal())
+bcsa = BeyondClassicSearchAlgorithm(p)
+path = csa.graph_depth_first_search(p.initialState())
+# csa.graph_breadth_first_search(p.initialState())
+# csa.graph_bidirectional_search(p.initialState(), p.goal())
+# csa.graph_uniform_cost_search(p.initialState())
+# bcsa.a_star(p.initialState())
+p.print_path(path)
